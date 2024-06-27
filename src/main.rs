7248ec9e3ce5de532db79ab5b801742ca0b823db
venysssssssssss <shablaur7@hotmail.com>
2024-06-26 21:49:41 -0300

    refactor: Add example_array function to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+
+
 fn main() {
     println!("Hello, world!");
+
+
+    example_array();
 }
+
+
+fn example_array() {
+    // Array
+    let arr: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("{:?}", arr);
+    // insert number 6 on the array
+    let mut arr: [i32; 6] = [0; 6];
+    arr[..5].copy_from_slice(&[1, 2, 3, 4, 5]);
+    arr[5] = 6;
+    println!("{:?}", arr);
+}
+
 
 
